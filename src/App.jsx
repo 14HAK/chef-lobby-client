872a6dde3c939d9f40b55243e90bd48da880e615
Dulon Mahadi <dulonmolla88@gmail.com>
2023-05-24 19:43:22 +0600

    client-side-ready-only demo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { FaCanadianMapleLeaf } from 'react-icons/fa';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>
+        Hello world! <FaCanadianMapleLeaf></FaCanadianMapleLeaf>
+      </h1>
+    </>
+  );
+}
+
+export default App;
